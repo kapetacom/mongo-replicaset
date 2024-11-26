d1c478d593b73bb325c8fe808bf96f5cb4ec3e10
Soren Mathiasen <sorenm@mymessages.dk>
2024-11-26 08:51:41 +0100

    making mongo quiet

---
+++ start-mongod.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Check if the QUIET environment variable is set to "true"
+if [ "$QUIET" = "true" ]; then
+    # Redirect all output to /dev/null, only keeping error output
+    exec mongod --replSet rs0 --bind_ip_all --keyFile /data/mongo-keyfile --quiet 2>&1 > /dev/null
+else
+    # Normal startup without suppressing output
+    exec mongod --replSet rs0 --bind_ip_all --keyFile /data/mongo-keyfile
+fi
