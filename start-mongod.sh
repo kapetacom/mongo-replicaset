5b762d5bf0288a5e61911be89807e30efa8be804
Soren Mathiasen <sorenm@mymessages.dk>
2024-12-16 08:58:35 +0100

    use correct entry file

--- start-mongod.sh
+++ start-mongod.sh
@@ -2,8 +2,8 @@
 # Check if the QUIET environment variable is set to "true"
 if [ "$QUIET" = "true" ]; then
     # Redirect all output to /dev/null, only keeping error output
-    exec mongod --replSet rs0 --bind_ip_all --keyFile /data/mongo-keyfile --quiet 2>&1 > /dev/null
+    exec /usr/local/bin/docker-entrypoint.sh --replSet rs0 --bind_ip_all --keyFile /data/mongo-keyfile --quiet 2>&1 > /dev/null
 else
     # Normal startup without suppressing output
-    exec mongod --replSet rs0 --bind_ip_all --keyFile /data/mongo-keyfile
+    exec /usr/local/bin/docker-entrypoint.sh --replSet rs0 --bind_ip_all --keyFile /data/mongo-keyfile
 fi
